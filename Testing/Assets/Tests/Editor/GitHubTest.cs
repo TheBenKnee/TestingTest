1bca9fbba01de6b320cb81667991a14b253a3ed5
TheBenKnee <54254870+TheBenKnee@users.noreply.github.com>
2023-01-31 18:01:14 -0800

    Adding first editor test

---
+++ Testing/Assets/Tests/Editor/GitHubTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Tests {
+	public class GitHubTest {
+
+		[Test]
+		public void EditmodeTestScripExampleSimplePasses () {
+
+		}
+
+		[UnityTest]
+		public IEnumerator EditmodeTestScriptExampleWithEnumeratorPasses () {
+			yield return null;
+		}
+
+	}
+}
